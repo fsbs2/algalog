f22dcd041853b9a2d8d979e46d9693fe3163529b
fsbs2 <gabriel_fsbs2@hotmail.com>
2023-05-18 03:11:44 -0300

    Crud implementation

--- src/main/java/com/algaworks/algalog/algalogapi/controllers/ClientController.java
+++ src/main/java/com/algaworks/algalog/algalogapi/controllers/ClientController.java
@@ -2,12 +2,10 @@ package com.algaworks.algalog.algalogapi.controllers;
 
 import com.algaworks.algalog.algalogapi.domain.model.Client;
 import com.algaworks.algalog.algalogapi.domain.repository.ClientRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +14,40 @@ import java.util.List;
 public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
+
     @GetMapping
     public List<Client> list() {
         return clientRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> findById(@PathVariable Long id) {
+        return clientRepository.findById(id)
+                .map(client -> ResponseEntity.ok(client))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client createClient(@RequestBody Client client) {
+        return clientRepository.save(client);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client client) {
+        if (!clientRepository.existsById(id))
+            return ResponseEntity.notFound().build();
+
+        client.setId(id);
+        client = clientRepository.save(client);
+        return ResponseEntity.ok(client);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Client> deleteClient(@PathVariable Long id) {
+        if (!clientRepository.existsById(id))
+            return ResponseEntity.notFound().build();
+        clientRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/algaworks/algalog/algalogapi/domain/model/Client.java
+++ src/main/java/com/algaworks/algalog/algalogapi/domain/model/Client.java
@@ -14,7 +14,6 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String Name;
     private String email;
 
